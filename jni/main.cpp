31f2dcba472fc7231ca00e6ad71b489944bb5f20
RedstoneGunMade <RedstoneGunMade@users.noreply.github.com>
2015-08-28 17:07:19 +0100

    Update main.cpp
    Add some things, an beacon tile

--- jni/main.cpp
+++ jni/main.cpp
@@ -5,27 +5,58 @@
 #include <Substrate.h>
 #include <string>
 
-#include <mcpe/gamemode/GameMode.h>
 #include <mcpe/client/resources/I18n.h>
-#include <mcpe/client/renderer/ItemInHandRenderer.h>
+#include <mcpe/item/Item.h>
+#include <mcpe/tile/Tile.h>
+#include <mcpe/client/renderer/TileTessellator.h>
 #include <mcpe/util/Matrix.h>
+#include <mcpe/client/renderer/ItemInHandRenderer.h>
 
 bool duelWield = false;
 
-//Change game version
+//Change game version.
 static std::string (*getGameVersionString_real)();
 static std::string getGameVersionString_hook() {
     return "PocketPC Addon v1.0";
 }
 
-//Change item names and edit the lang
+//Change item names and edit the lang.
 static std::string (*I18n_get_real)(std::string const&, std::vector<std::string, std::allocator<std::string>> const&);
 static std::string I18n_get_hook(std::string const& key, std::vector<std::string, std::allocator<std::string>> const& a) {
 	if(key == "menu.copyright") return "©SmartDEV";
+	if(key == "menu.play") return "Singleplayer";
 	return I18n_get_real(key, a);
 }
 
-//Duel wielding
+//TileTesellation.
+bool (*_TileTessellator$tessellateInWorld)(TileTessellator*, Tile*, const TilePos&, bool);
+bool TileTessellator$tessellateInWorld(TileTessellator* self, Tile* tile, const TilePos& pos, bool b) {
+	switch(tile->id) {
+		case 138:
+			return self->tessellateBeaconInWorld(tile, pos);
+			break;
+		default:
+			return _TileTessellator$tessellateInWorld(self, tile, pos, b);
+			break;
+	}
+}
+
+//Tile Init.
+void (*_Tile$initTiles)();
+void Tile$initTiles() {
+	_Tile$initTiles();
+	Tile* beacon = new Tile(138, "glass", &Material::glass);
+	beacon->setNameId("Beacon");
+	Tile::tiles[138] = beacon;
+	Tile::solid[138] = false;
+	beacon->setSoundType(Tile::SOUND_GLASS);
+	Tile::lightBlock[138] = 0;
+	new TileItem(138 - 0x100);
+	beacon->Tile::glass->renderPass;
+
+}
+
+//Duel wielding.
 static void (*_ItemInHandRenderer$render)(ItemInHandRenderer*, float);
 static void ItemInHandRenderer$render(ItemInHandRenderer* renderer, float partialTicks) {
 	if(duelWield){
@@ -37,12 +68,13 @@ static void ItemInHandRenderer$render(ItemInHandRenderer* renderer, float partia
 	}
 }
 
-//Hook the functions :)
+//Hook the functions.
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 	void* handle = dlopen("libminecraftpe.so", RTLD_LAZY);
 	void* getGameVersionString = dlsym(RTLD_DEFAULT, "_ZN6Common20getGameVersionStringEv");
 	MSHookFunction(getGameVersionString, (void*)&getGameVersionString_hook, (void**)&getGameVersionString_real);
 	MSHookFunction((void*) &ItemInHandRenderer::render, (void*) &ItemInHandRenderer$render, (void**) &_ItemInHandRenderer$render);
 	MSHookFunction((void*) &I18n::get, (void*) &I18n_get_hook, (void**) &I18n_get_real);
-	return JNI_VERSION_1_2;
+	MSHookFunction((void*) &Tile::initTiles, 					(void*) &Tile$initTiles, 						(void**) &_Tile$initTiles);
+	MSHookFunction((void*) &TileTessellator::tessellateInWorld, (void*) &TileTessellator$tessellateInWorld, 	(void**) &_TileTessellator$tessellateInWorld);
 }
